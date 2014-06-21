f1b6bd273badd68662e9ce3457cac2cd44f9a01f
Jon Kuperman <jon.kuperman@gmail.com>
2014-06-20 21:19:35 -0700

    added MIT license

--- twitter.intents.php
+++ twitter.intents.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Twitter Intents
-Plugin URI: http://www.planet-interactive.co.uk/simple-twitter-tweets
+Plugin URI: https://github.com/jkup/twitter-intents
 Description: Add Twitter intents functionality to your blog. Easily let your readers reply, retweet, or favorite your posts!
 Author: Jon Kuperman
 Version: 0.1.0
 Author URI: http://jonkuperman.com
 */
 
-class PI_SimpleTwitterTweets extends WP_Widget{
+class TwitterIntents extends WP_Widget{
 };
